90a7767dc99fb36a79492c8efddf37acf4716afe
Muriithi Ian <imuriithiian@gmail.com>
2022-08-16 11:51:53 +0300

    completed

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
   // Write your code here!
+  if (feet > 2500) {
+    return "No can do.";
+  }
+  if (feet > 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
+  if (feet < 400) {
+    return "This one is on me!";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(arg) {
   // Write your code here!
+  if (arg === "NYC") {
+    return "Ok, sounds good.";
+  }
+  if (arg === "Pittsburgh") {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(arg) {
   // Write your code here!
+  switch (arg) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
+
+// console.log(scuberGreetingForFeet(2501));
